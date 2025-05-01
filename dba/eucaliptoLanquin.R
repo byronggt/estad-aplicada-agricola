92c731ab61763440fb777bcc2f4ef06420f8055b
unknown <byrong.gt@gmail.com>
2025-05-01 12:01:12 -0600

    Actualizado DBA eucalipto Lankin

---
+++ dba/eucaliptoLanquin.R
@@ -0,0 +1,6 @@
+# Dr. Byron González
+# http://byrong.cc
+
+# Detalles del experimento disponibles en la página 83
+# del libro de texto "Diseño y análisis de experimentos" de
+# López B. y González B. (2016) CETE, Facultad de Agronomía
