6a76f53ed0e9c4c4dd9f40514926816f6a39dacd
unknown <byrong.gt@gmail.com>
2025-05-01 13:55:42 -0600

    Actualizado brócoli combinatorio

--- arreglo combinatorio/brocoli_combinatorio.R
+++ arreglo combinatorio/brocoli_combinatorio.R
@@ -6,4 +6,27 @@ if(!require(performance)){install.packages("performance")}
 
 fact<- fread("https://archive.org/download/Brocoli/Brocoli.txt",header=T, sep="\t", dec=",")
 
+attach(fact)
+head(fact)
+hibrido<-factor(Hib)
+distanciamiento<-factor(Dist)
+bloque<-factor(Bloque)
+ren<-as.vector(Rend)
+rendimiento<-as.numeric(ren)
 
+# Gráfico de la interacción entre factores
+interaction.plot( distanciamiento,hibrido,rendimiento, fixed=T, xlab="Distanciamiento", ylab="Rendimiento",col = "blue")
+
+# Análisis de varianza en arreglo combinatorio para ambos factores
+resultado<-lm(rendimiento~bloque+hibrido*distanciamiento)
+anova(resultado)
+
+# Revisión de los supuestos del modelo
+par(mfrow = c(2,2))
+plot(resultado)
+check_normality(resultado)
+win.graph(11,11)
+check_model(resultado)
+
+# Prueba múltiple de medias bajo el criterio de Scott Knott
+with(fact,FAT2DBC(hibrido,distanciamiento,bloque,rendimiento,mcomp = "sk"))
