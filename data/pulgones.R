2fe64f61e2766c58aa797bf039b2ab6452fc1807
unknown <byrong.gt@gmail.com>
2025-05-01 11:40:54 -0600

    Actualizado dca pulgones

--- data/pulgones.R
+++ data/pulgones.R
@@ -3,6 +3,7 @@
 
 if(!require(data.table)){install.packages("data.table")}
 if(!require(performance)){install.packages("performance")}
+if(!require(car)){install.packages("car")}
 
 DICt<- fread("https://archive.org/download/pulgones/pulgones.txt",header=T, sep="\t", dec=",")
 head(DICt)
@@ -20,11 +21,34 @@ resultado<-lm(pulg~trat)
 anova(resultado)
 
 # Revisión de los supuestos del Andeva
-win.graph(11,11)
-check_model(resultado)
-check_normality(resultado) # 
+par(mfrow = c(2,2))
+plot(resultado)
+check_normality(resultado)
 
-# Transformación de la variable original
+# Transformación de la variable original mediante la familia de Box-Cox
+summary(powerTransform(DICt$pulgones)) # Aplicar log
+
+# Realizar el Andeva con la variable transformada
+logpulg<-log(pulg)
+resultado1<-lm(logpulg~trat)
+anova(resultado1)
+
+# Revisión de supuestos con la variable transformada
+par(mfrow = c(2,2))
+plot(resultado1)
+check_normality(resultado1)
+
+# Prueba múltiple de medias bajo el criterio de Scott Knott
+with(DICt,DIC(trat,logpulg,mcomp = "sk"))
+
+
+
+
+
+
+
+
+
 
 
 
